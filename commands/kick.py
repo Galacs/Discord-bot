8d3f87b68ec2a25c4d11e81485da6921f6093419
Galacs <34189296+Galacs@users.noreply.github.com>
2020-03-14 10:13:37 +0100

    Added kick reason with player name who kicked
    Added kick reason with player name who kicked

--- commands/kick.py
+++ commands/kick.py
@@ -10,7 +10,7 @@ class kick(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def kickCmd (self, ctx, member: discord.Member=None, arg=""):
         await ctx.message.delete()
-        await member.kick()
+        await member.kick(reason=f"Kick par {str(ctx.message.author)}")
         if arg == "":
             await ctx.send(f"{ctx.author.mention} a kick {member.mention}")
         elif arg == 's':
