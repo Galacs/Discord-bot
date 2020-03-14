12fb6836deed5324ba81c5c99048f458ef901ef6
Galacs <34189296+Galacs@users.noreply.github.com>
2020-03-14 11:21:50 +0100

    Created custom messages using json files
    Created custom messages using json files, every servers has a .json file

---
+++ commands/msg.py
@@ -0,0 +1,48 @@
+import discord, json, os
+from discord.ext import commands
+def getmsg(guild, msg):
+    return json.load(open(f'./servers/{guild.id}.json'))[msg]
+
+class msg(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for guild in self.bot.guilds:
+            data = json.load(open(f'./servers/{guild.id}.json'))
+            data["server_name"] = guild.name
+            json.dump(data, open(f"./servers/{guild.id}.json", "w"))
+
+    @commands.command(name="showmsg")
+    @commands.has_permissions(administrator=True)
+    async def msgCmd(self, ctx, msg: str):
+        data = json.load(open(f'./servers/{ctx.guild.id}.json'))
+        try:
+            await ctx.send(data[msg])
+        except KeyError:
+            await ctx.send("Aucun message n'a été defini")
+        
+    
+    @commands.command(name="setmsg")
+    @commands.has_permissions(administrator=True)
+    async def setmsgCmd(self, ctx, response: str,message: str):
+        data = json.load(open(f'./servers/{ctx.guild.id}.json'))
+        data[response] = message
+        json.dump(data, open(f"./servers/{ctx.guild.id}.json", "w"))
+        await ctx.send(f"Le message {response} a été changé pour {message}")
+
+    @commands.command(name="setdefault")
+    @commands.has_permissions(administrator=True)
+    async def setdefault(self, ctx, msg):
+        data = json.load(open(f'./servers/{ctx.guild.id}.json'))
+        del data[msg]
+        json.dump(data, open(f"./servers/{ctx.guild.id}.json", "w"))
+        await ctx.send(f"Le message {msg} a étais remis à defaut")
+    @commands.command(name="showallmsg")
+    @commands.has_permissions(administrator=True)
+    async def showallmsg(self, ctx):
+        await ctx.send(json.load(open(f'./servers/{ctx.guild.id}.json')))
+
+def setup(bot):
+    bot.add_cog(msg(bot))
