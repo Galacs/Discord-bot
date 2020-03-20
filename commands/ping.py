89b275118bf4235fbd9fe60c53e89ed51c8ea66c
Galacs <34189296+Galacs@users.noreply.github.com>
2020-03-20 11:03:41 +0100

    Reverted to inital lang system
    Reverted to inital lang system

---
+++ commands/ping.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from commands.msg import getmsg
+
+class ping(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping")
+    async def pingCmd(self, ctx):
+        await ctx.message.delete()
+        try:
+            await ctx.send(getmsg(ctx.guild, "ping").format(latency=round(int(self.bot.latency*1000), 1)))
+        except KeyError:
+            await ctx.send('Pong! Mon ping est de {0}ms'.format(round(int(self.bot.latency*1000), 1)))
+        return
+
+def setup(bot):
+    bot.add_cog(ping(bot))
